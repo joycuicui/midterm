96c58a9788c40a633afe225b7f39008a2684568e
c22quiambao <christelquiambao@gmail.com>
2023-12-18 23:26:54 -0800

    added new file to hold drop and create statements.

---
+++ db/schema/03_plane_photos.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS plane_photos CASCADE;
+CREATE TABLE plane_photos (
+id SERIAL PRIMARY KEY NOT NULL,
+planes_id INTEGER,
+img_path1 TEXT NOT NULL,
+img_path2 TEXT NOT NULL,
+image_path3 TEXT NOT NULL
+);
