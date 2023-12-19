a368dcbe8265e54fcaa9177e10c10ec0459e1536
c22quiambao <christelquiambao@gmail.com>
2023-12-19 14:21:07 -0800

    deleted sql files since they are no longer needed

--- db/schema/03_plane_photos.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS plane_photos CASCADE;
-CREATE TABLE plane_photos (
-id SERIAL PRIMARY KEY NOT NULL,
-planes_id INTEGER,
-img_path1 TEXT NOT NULL,
-img_path2 TEXT NOT NULL,
-img_path3 TEXT NOT NULL
-);
