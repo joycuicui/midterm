5656275174f2028b73d45aa0f8a6cd626e1ff176
c22quiambao <christelquiambao@gmail.com>
2023-12-18 23:27:52 -0800

    renamed files for clarity and added more lines of insert statements.

---
+++ db/seeds/01_users_seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (name, email, password) VALUES
+('Joy','joy@email.com','abc123'),
+('Christel','christel@email.com', 'abc123'),
+('Karan','koran@email.com', 'abc123'),
+('Alice','alice@email.com', 'abc123'),
+('Kira','kira@email.com', 'abc123'),
+('Ethan','ethan@email.com', 'abc123'),
+('Olivia','olivia@email.com', 'abc123'),
+('Liam','liam@email.com', 'abc123'),
+('Ava','ava@email.com', 'abc123');
---
+++ db/seeds/03_plane_photos_seed.sql
@@ -0,0 +1,105 @@
+INSERT INTO plane_photos (
+planes_id,
+img_path1,
+img_path2,
+image_path3)
+VALUES ( 1,
+'db/images/1/1956_bonanza_1.jpg',
+'db/images/1/1956_bonanza_2.jpg',
+'db/images/1/1956_bonanza_3.jpg'),
+( 2,
+'db/images/2/1982_mooney_1.jpg',
+'db/images/2/1982_mooney_2.jpg',
+'db/images/2/1982_mooney_3.jpg'
+),
+( 3,
+'db/images/3/1973_piper_1.jpg',
+'db/images/3/1973_piper_2.jpg',
+'db/images/3/1973_piper_3.jpg'
+),
+( 4,
+'db/images/4/1975_beechcraft_1.jpg',
+'db/images/4/1975_beechcraft_2.jpg',
+'db/images/4/1975_beechcraft_3.jpg'
+),
+( 5,
+'db/images/5/1966_mooney_1.jpg',
+'db/images/5/1966_mooney_2.jpg',
+'db/images/5/1966_mooney_3.jpg'
+),
+( 6,
+'db/images/6/2006_cirrus_1.jpg',
+'db/images/6/2006_cirrus_2.jpg',
+'db/images/6/2006_cirrus_3.jpg'
+),
+( 7,
+'db/images/7/1999_cessna_1.jpg',
+'db/images/7/1999_cessna_2.jpg',
+'db/images/7/1999_cessna_3.jpg'
+),
+( 8,
+'db/images/8/1978_cessna_1.jpg',
+'db/images/8/1978_cessna_2.jpg',
+'db/images/8/1978_cessna_3.jpg'
+),
+( 9,
+'db/images/9/1965_cessna_1.jpg',
+'db/images/9/1965_cessna_2.jpg',
+'db/images/9/1965_cessna_3.jpg'
+),
+( 10,
+'db/images/10/1972_piper_1.jpg',
+'db/images/10/1972_piper_2.jpg',
+'db/images/10/1972_piper_3.jpg'
+),
+( 11,
+'db/images/11/1976_beechcraft_1.jpg',
+'db/images/11/1976_beechcraft_2.jpg',
+'db/images/11/1976_beechcraft_3.jpg'
+),
+( 12,
+'db/images/12/2002_cessna_1.jpg',
+'db/images/12/2002_cessna_2.jpg',
+'db/images/12/2002_cessna_3.jpg'
+),
+( 13,
+'db/images/13/2006_beechcraft_1.jpg',
+'db/images/13/2006_beechcraft_2.jpg',
+'db/images/13/2006_beechcraft_3.jpg'
+),
+( 14,
+'db/images/14/1968_piper_1.jpg',
+'db/images/14/1968_piper_2.jpg',
+'db/images/14/1968_piper_3.jpg'
+),
+( 15,
+'db/images/15/1983_piper_1.jpg',
+'db/images/15/1983_piper_2.jpg',
+'db/images/15/1983_piper_3.jpg'
+),
+( 16,
+'db/images/16/1940_messerschmitt_1.jpg',
+'db/images/16/1940_messerschmitt_2.jpg',
+'db/images/16/1940_messerschmitt_3.jpg'
+),
+( 17,
+'db/images/17/1943_north_1.jpg',
+'db/images/17/1943_north_2.jpgg',
+'db/images/17/1943_north_3.jpg'
+),
+( 18,
+'db/images/18/1942_noorduyn_1.jpg',
+'db/images/18/1942_noorduyn_2.jpg',
+'db/images/18/1942_noorduyn_3.jpg'
+),
+( 19,
+'db/images/19/1997_yakovlev_1.jpg',
+'db/images/19/1997_yakovlev_2.jpg',
+'db/images/19/1997_yakovlev_3.jpg'
+),
+( 20,
+'db/images/20/1951_chance_1.jpg',
+'db/images/20/1951_chance_2.jpg',
+'db/images/20/1951_chance_3.jpg'
+);
+
