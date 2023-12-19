55170ce7f1ce8a778e1bab0cbd2a0c5ca377bd0f
c22quiambao <christelquiambao@gmail.com>
2023-12-18 23:22:08 -0800

    created schema sql file to hold drop and create statements. Created 05_messages_seed.sql for intial commit of seeding data.

---
+++ db/seeds/05_messages_seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO messages (listing_id, sender_id, receiver_id, content, time_sent)
+VALUES
+( 10,
+  4,
+  1,
+  'Hi! I would like to inquire if the price can be negotiated for the item posted.',
+  '2023-12-15 08:30'),
+( 10,
+  1,
+  4, 'The price is negotiable. How much are willing to negotiate?', '2023-12-15 09:30'),
+( 10,
+  4,
+  1,
+  'How about 10% off the selling price?',
+  '2023-12-15 14:30'),
+( 20,
+  6,
+  3,
+  'Why is this so expensive?',
+  '2023-01-22 15:30');
