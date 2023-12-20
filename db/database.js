54b19500724b342fbb3783b73d9becd8f8f6bc54
joycuicui <cuiqijoy@outlook.com>
2023-12-19 19:48:52 -0800

    fix errors

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const db = require("../db/connection");
 /// USERS
 ////////////////////////////////////////////////////////////////////////////////////////////////
 const getUserWithEmail = function (email) {
-  return pool
+  return db
     .query(`SELECT * FROM users WHERE email = $1;`, [email])
     .then((result) => {
       // console.log("result.rows:", result.rows);
@@ -18,7 +18,7 @@ const getUserWithEmail = function (email) {
 };
 
 const getUserWithId = function (id) {
-  return pool
+  return db
     .query(`SELECT * FROM users WHERE id = $1;`, [id])
     .then((result) => {
       // console.log("result.rows:", result.rows);
@@ -32,7 +32,7 @@ const getUserWithId = function (id) {
 };
 
 const addUser = function (user) {
-  return pool
+  return db
     .query(
       `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
       [user.name, user.email, user.password]
--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,81 +1,81 @@
-$(() => {
-  window.header = {};
+// $(() => {
+//   window.header = {};
 
-  const $header = $(".header");
+//   const $header = $(".header");
 
-  let currentUser = null;
+//   let currentUser = null;
 
-  function updateHeader(user) {
-    console.log("updating header with user:", user);
-    currentUser = user;
-    $header.find(".nav-bar").remove();
-    let navBar;
-    if (!user) {
-      navBar = `
-      <div class="nav-box">
-        <div class="logo">
-          <i class="fa-solid fa-plane-up"></i>
-        </div>
-      <nav class="nav-bar">
-          <ul class="nav-bar-items">
-            <li class="home">Home</li>
-            <li class="search">Search</li>
-            <li class="login">Log In</li>
-            <li class="signup">Sign Up</li>
-          </ul>
-        </nav>
-      `;
-    } else {
-      navBar = `
-      <div class="nav-box">
-        <div class="logo">
-          <i class="fa-solid fa-plane-up"></i>
-        </div>
-      <nav class="nav-bar">
-          <ul class="nav-bar-items">
-            <li class="home">Home</li>
-            <li class="search">Search</li>
-            <li class="sell">Sell</li>
-            <li class="my-listings">Listings</li>
-            <li class="my-likes">Favorites</li>
-            <li class="messages">Messages</li>
-            <li class="logout">Log out</li>
-          </ul>
-        </nav>
-      `;
-    }
-    $header.append(navBar);
-  }
-  window.header.update = updateHeader;
+//   function updateHeader(user) {
+//     console.log("updating header with user:", user);
+//     currentUser = user;
+//     $header.find(".nav-bar").remove();
+//     let navBar;
+//     if (!user) {
+//       navBar = `
+//       <div class="nav-box">
+//         <div class="logo">
+//           <i class="fa-solid fa-plane-up"></i>
+//         </div>
+//       <nav class="nav-bar">
+//           <ul class="nav-bar-items">
+//             <li class="home">Home</li>
+//             <li class="search">Search</li>
+//             <li class="login">Log In</li>
+//             <li class="signup">Sign Up</li>
+//           </ul>
+//         </nav>
+//       `;
+//     } else {
+//       navBar = `
+//       <div class="nav-box">
+//         <div class="logo">
+//           <i class="fa-solid fa-plane-up"></i>
+//         </div>
+//       <nav class="nav-bar">
+//           <ul class="nav-bar-items">
+//             <li class="home">Home</li>
+//             <li class="search">Search</li>
+//             <li class="sell">Sell</li>
+//             <li class="my-listings">Listings</li>
+//             <li class="my-likes">Favorites</li>
+//             <li class="messages">Messages</li>
+//             <li class="logout">Log out</li>
+//           </ul>
+//         </nav>
+//       `;
+//     }
+//     $header.append(navBar);
+//   }
+//   window.header.update = updateHeader;
 
-  // testing: when there is a user logged in
-  const testUser = { name: "abc" };
-  // testing: when there is no user logged in
-  // const testUser = null;
-  window.header.update(testUser);
+//   // testing: when there is a user logged in
+//   const testUser = { name: "abc" };
+//   // testing: when there is no user logged in
+//   // const testUser = null;
+//   window.header.update(testUser);
 
-  ////////////////////////////////////////////////////////////////////////
-  /// event listeners for header elements: home, search, myListing...
-  ////////////////////////////////////////////////////////////////////////
-  $("$header").on("click", ".home", function () {
-    // 1. clear listings
-    // 2. get all listings
-    // 3. show listings
-  });
+//   ////////////////////////////////////////////////////////////////////////
+//   /// event listeners for header elements: home, search, myListing...
+//   ////////////////////////////////////////////////////////////////////////
+//   $("$header").on("click", ".home", function () {
+//     // 1. clear listings
+//     // 2. get all listings
+//     // 3. show listings
+//   });
 
-  $("$header").on("click", ".search", function () {});
+//   $("$header").on("click", ".search", function () {});
 
-  $("$header").on("click", ".sell", function () {});
+//   $("$header").on("click", ".sell", function () {});
 
-  $("$header").on("click", ".my-listings", function () {
-    // 1. clear listings
-    // 2. get all listings(pass user_id as argument)
-    // 3. show listings
-  });
+//   $("$header").on("click", ".my-listings", function () {
+//     // 1. clear listings
+//     // 2. get all listings(pass user_id as argument)
+//     // 3. show listings
+//   });
 
-  $("$header").on("click", ".my-likes", function () {});
-  $("$header").on("click", ".messages", function () {});
-  $("$header").on("click", ".login", function () {});
-  $("$header").on("click", ".signup", function () {});
-  $("$header").on("click", ".logout", function () {});
-});
+//   $("$header").on("click", ".my-likes", function () {});
+//   $("$header").on("click", ".messages", function () {});
+//   $("$header").on("click", ".login", function () {});
+//   $("$header").on("click", ".signup", function () {});
+//   $("$header").on("click", ".logout", function () {});
+// });
